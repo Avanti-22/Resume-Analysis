40020f4c8e90dd331e6cee5c7411082b2eaebf11
Avanti-22 <thaleavanti2002@gmail.com>
2023-09-01 10:12:57 +0530

    Arranged the directories and created the Django project
    added all the resumes to a folder
    created a new folder for extracted data
    started the django project
    created two separate directories one for resume processing and other for django app

---
+++ ResumeSite/webapp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+# Create your models here.
+class ResumeData(models.Model):
+    Resume_No = models.IntegerField(primary_key = True)
+    Name = models.CharField(max_length=200)
+    Email = models.EmailField("")
+    Mobile_No = models.IntegerField()
+    Skills = models.TextField(max_length=200)
+    Education = models.TextField(max_length=200)
+    Experience = models.TextField(max_length=200)
+
+class Matched(models.Model):
+    Resume = models.ForeignKey(ResumeData, on_delete=models.CASCADE)
+    Extracted_skills = models.TextField()
+    Resuired_skills = models.TextField()
+    Matched_skills = models.TextField()
+    Percent_matched = models.DecimalField(decimal_places=2,max_digits=3)
+    
+class JobDescription(models.Model):
+    Title = models.CharField(max_length=200)
+    Required_Skills = models.TextField()
+    Requires_Experience = models.CharField(max_length=200)
